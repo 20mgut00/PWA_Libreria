45fb7ea062241a88c283e6149d1cddc158c1fef9
Miguel Gutierrez Vazquez <miggut02@ucm.es>
2022-04-11 16:05:15 +0200

    prueba

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Book search</h1>
       </header>
+      <body>
+        <input type="text" name="author"></input>
+        <input type="text" name="title"></input>
+        
+      </body>
     </div>
   );
 }
